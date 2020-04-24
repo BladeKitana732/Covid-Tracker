7ef09d6e982e10187a877ba4087f48cfdd13c63a
Teresa Malave <dev.malave7@gmail.com>
2020-04-24 11:00:08 -0500

    finally got ONLY country arrays to display in console

--- covid-tracker/src/components/PropTest.js
+++ covid-tracker/src/components/PropTest.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function PropTest(props) {
-    console.log(props.infoDisplay)
+    console.log(props.infoDisplay.map(country => console.log(country)))
     return(
         <select>
 

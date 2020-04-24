45b2f919e74e72d8cb79eaf604ac9952eb1c3614
Teresa Malave <dev.malave7@gmail.com>
2020-04-24 17:52:59 -0500

    getting undefined; tried to push same as when i got names to display

--- covid-tracker/src/components/DataDisplay.js
+++ covid-tracker/src/components/DataDisplay.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import DropDown from './DropDown';
+// import DropDown from './DropDown';
 
 
 
-export default function DataDisplay() {
+
+export default function DataDisplay(props) {
+
+    console.log(props.infoDisplay)
+
+
     return (
         <div>
-               <DropDown />  
-               <ul style={{textDecoration: "none"}}>
-                    <li>New confirmed cases:</li>
-                    <li>Total confirmed cases:</li>
-                    <li>New death cases:</li>
-                    <li>Total death count:</li>
-                    <li>Newly recovered today:</li>
-                    <li>Total recovered count:</li>
-                </ul>
+               {/* <DropDown />   */}
+             
         </div>
     )
 }

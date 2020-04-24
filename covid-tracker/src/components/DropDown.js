cfee557a408ed7ee96a719e57dd2d1ed090d5b70
Teresa Malave <dev.malave7@gmail.com>
2020-04-23 22:52:42 -0500

    nested dropdown component with landing page component

--- covid-tracker/src/components/DropDown.js
+++ covid-tracker/src/components/DropDown.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class DropDown extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
 
         this.state = {
             country: "",
@@ -14,6 +15,28 @@ export default class DropDown extends Component {
             totalRecovered: ""
         }
     }
+
+
+    componentDidMount() {
+        const currentInfo = "https://api.covid19api.com/summary";
+        
+        axios.get(currentInfo)
+  
+        .then((response) => {
+            let data = response.data.Countries;
+  
+            console.log(data)
+  
+            //how to set to if selection in drop list === this setState country then to render modal and submission of form
+            this.setState({
+                country: data
+            })
+        })
+      
+  
+      }
+
+
     render() {
         return (
             <div>
--- covid-tracker/src/components/LandingPage.js
+++ covid-tracker/src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import DropDown from './DropDown';
+// import axios from 'axios';
 
 export default class LandingPage extends Component {
   
@@ -64,40 +65,28 @@ export default class LandingPage extends Component {
     }
 
 
-    componentDidMount() {
-      const currentInfo = "https://api.covid19api.com/summary";
+    // componentDidMount() {
+    //   const currentInfo = "https://api.covid19api.com/summary";
       
-      axios.get(currentInfo)
+    //   axios.get(currentInfo)
 
-      .then((response) => {
-          let data = response.data.Countries;
+    //   .then((response) => {
+    //       let data = response.data.Countries;
 
-          console.log(data)
+    //       console.log(data)
 
-          //how to set to if selection in drop list === this setState country then to render modal and submission of form
-          this.setState({
-              country: data
-          })
-      })
+    //       //how to set to if selection in drop list === this setState country then to render modal and submission of form
+    //       this.setState({
+    //           country: data
+    //       })
+    //   })
     
 
-    }
+  
     
 
     render() {
   
-      // const dropDownListOptions = this.state.country.map((name) => {
-      //   return (
-      //     <div>
-      //     <p>Select Home Country:
-      //     <select>
-      //      <option></option>
-      //     </select>
-      //     </p>
-      //     </div>
-
-      //   )
-      // })
 
 
 
@@ -121,16 +110,8 @@ export default class LandingPage extends Component {
                     <input onChange= {this.handleEmail} type="text" email="email" value={this.state.email}/>
                   </label>
 
-                  {/* Drop down list of countries to select from here-- Completed*/}
-                  
-                  {/* <p>Select Home Country:
-                  <select>
-                   
-                  </select>
-                  </p>
-
-
-                    <input type="submit" value="submit" /> */}
+                  <DropDown />
+        
                 </form>
             </div>
         )

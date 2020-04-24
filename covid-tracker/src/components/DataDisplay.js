7bfa94381e44fb355d24f9aa9cbe7900d8d1a67c
Teresa Malave <dev.malave7@gmail.com>
2020-04-24 18:10:11 -0500

    referencing my wine api to get to path needed to display info properly; went back to api doumentation i am using to find paths

--- covid-tracker/src/components/DataDisplay.js
+++ covid-tracker/src/components/DataDisplay.js
@@ -6,6 +6,16 @@ import axois from 'axios';
 import PropTest from './PropTest';
 
 
+//resource found: https://www.youtube.com/watch?v=12l6lkW6JhE&t=564s - regarding the axios.create()
+//resource found: https://www.codota.com/code/javascript/functions/axios/create
+
+//creating a api variable as base to be able to pull information from the separate objects in unison
+
+let wineLink = axios.create({
+    baseURL: "http://myapi-profstream.herokuapp.com/api/2f49a6/wines"
+})
+
+
 export default class DataDisplay extends Component {
     render() {
         return (
